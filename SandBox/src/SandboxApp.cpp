78248f024186d34a6c7cc1b62d28b579d6d135bf
Lazy <15284162817@163.com>
2026-08-21 22:45:05 +0800

    重构项目结构：源码移至 Flame/ 目录，添加 SandBox，清理构建产物

---
+++ SandBox/src/SandboxApp.cpp
@@ -0,0 +1,19 @@
+#include <Flame.h>
+
+class Sandbox : public Flame::Application
+{
+public:
+	Sandbox()
+	{
+	}
+
+	~Sandbox()
+	{
+
+	}
+};
+
+Flame::Application* Flame::CreateApplication()
+{
+	return new Sandbox();
+}
